b331c3faffe3d56a925c6f10705743453ba96f3d
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-12-01 15:25:19 -0300

    feat: Add API base URL configuration.

--- front/src/api/config.ts
+++ front/src/api/config.ts
@@ -1,3 +1,2 @@
 // src/api/config.ts
-export const API_BASE_URL = 'http://ecohouse-env.eba-vay8q3u6.us-east-1.elasticbeanstalk.com';
-
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
