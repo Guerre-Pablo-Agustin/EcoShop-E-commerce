f3d815d5a329fd78f81d70343b5f12dee04eecaf
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-12-05 20:54:22 -0300

    feat: add Category and CreateCategoryDto interfaces

--- front/src/types/Category.types.ts
+++ front/src/types/Category.types.ts
@@ -2,10 +2,14 @@ export interface Category {
   id: number;
   name: string;
   description: string;
-  isActive: boolean;
+  iconUrl: string;
+  parentCategoryId: number;
+  subCategories: string[];
 }
 
 export interface CreateCategoryDto {
   name: string;
   description: string;
+  iconUrl: string;
+  parentCategoryId: number;
 }
