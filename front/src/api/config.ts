0101b7912cd9f28a087d0d26f7e172017d2cf6b8
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-12-11 01:24:46 -0300

    feat: Introduce API configuration with base URL and environment logging.

--- front/src/api/config.ts
+++ front/src/api/config.ts
@@ -1,6 +1,5 @@
 // src/api/config.ts
-export const API_BASE_URL = import.meta.env.VITE_API_BASE
-
+export const API_BASE_URL = import.meta.env.VITE_API_BASE || '';
 
 console.log("API_BASE_URL",API_BASE_URL);
 console.log('🌍 Modo:', import.meta.env.MODE); // "development" o "production"
